af36d5fc21cb986a473f065d55de978466d1746d
Rudresh Gupta <rudreshgpt7@gmail.com>
2021-04-17 15:43:05 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -4,26 +4,13 @@ import Navbar from "./components/navbar";
 import Slider from "./components/slider";
 
 function App() {
-<<<<<<< HEAD
-  return (
-    <div>
-      <Navbar/>
-    </div>
 
-
-  );
-=======
     return (
         <React.Fragment>
-            <div>
-                <Navbar />
-            </div>
-            <div>
-                <Slider />
-            </div>
+          <Navbar/>
+          <Slider/>
         </React.Fragment>
     );
->>>>>>> 9fb33f19fc5c2c639e220300484d39010da7113f
 }
 
 export default App;
