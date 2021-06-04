d58bd68a1a04a1d5ed5da1cc13c8514e894b7f4a
Abhishek <abhic9236@gmail.com>
2021-06-04 23:23:26 +0530

    Minor Changes

--- src/components/pages/House.js
+++ src/components/pages/House.js
@@ -1,16 +1,28 @@
+import product
+
 function House(props) {
-    return (
-        <div>
-            {console.log(props)}
-            <h1>House component</h1>
-            <h6>property_type<strong>{props.item.property_type}</strong></h6>
-            <h6>title<strong>{props.item.title}</strong></h6>
-            {/* <h6>description<strong>{props.item.description}</strong></h6> */}
-            <h6>bedroom<strong>{props.item.bedroom}</strong></h6>
-            <h6>owner_name<strong>{props.item.owner_name}</strong></h6>
-            <h6>pin<strong>{props.item.pin}</strong></h6>
-        </div>
-    );
+	return (
+		<div class="box">
+            <div class="image">
+                <li><img src = "#"></img></li>
+            </div>
+		</div>
+	);
 }
 
 export default House;
+
+{
+	/* {console.log(props)} */
+}
+{
+	/* <h1>House component</h1>
+            <h6>property_type<strong>{props.item.property_type}</strong></h6>
+            <h6>title<strong>{props.item.title}</strong></h6>
+            {/* <h6>description<strong>{props.item.description}</strong></h6> */
+}
+{
+	/* <h6>bedroom<strong>{props.item.bedroom}</strong></h6>
+            <h6>owner_name<strong>{props.item.owner_name}</strong></h6>
+            <h6>pin<strong>{props.item.pin}</strong></h6> */
+}
